0236cfbb2be4bcff26b30db7944e20b59d435b4e
pc <2434491564@qq.com>
2024-12-11 03:11:07 +0800

    update

--- frontend/src/pages/cats/catBase.vue
+++ frontend/src/pages/cats/catBase.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container>
-    <v-chip-group absolute top left v-if="user.login">
+    <v-chip-group absolute top left v-if="user.login && !user.is_superuser">
       <v-chip>
         剩余罐罐: {{ remainingCans }}
       </v-chip>
     </v-chip-group>
     <v-row>
-      <v-col cols="12" md="4" v-for="cat in cats" :key="cat.id">
+      <v-col cols="12" md="6" v-for="cat in cats" :key="cat.id">
         <v-card class="d-flex flex-column text-center">
         <!-- <v-card lass="d-flex flex-column"> -->
           <v-img
@@ -14,32 +14,78 @@
             height="200px"
             class="mx-auto"
           ></v-img>
-          <v-card-title class="headline" @click="goToCatDetails(cat.id)">{{ cat.name }}</v-card-title>
-          <v-card-subtitle @click="goToCatDetails(cat.id)">年龄: {{ cat.age }}</v-card-subtitle>
-          <v-card-subtitle @click="goToCatDetails(cat.id)">性别: {{ cat.is_male ? '男' : '女' }}</v-card-subtitle>
-          <v-card-subtitle @click="goToCatDetails(cat.id)">描述: {{ cat.description }}</v-card-subtitle>
-          <v-card-subtitle @click="goToCatDetails(cat.id)">健康状况: {{ cat.health_condition  == 1 ? "HEALTHY" : cat.health_condition == 2 ? "SICK" : cat.health_condition == 3 ? "VACCINATED" : "DEAD"}}</v-card-subtitle>
+          <v-card-title class="headline">{{ cat.name }}</v-card-title>
+          <v-card-subtitle >年龄: {{ cat.age }}</v-card-subtitle>
+          <v-card-subtitle >性别: {{ cat.is_male ? '男' : '女' }}</v-card-subtitle>
+          <v-card-subtitle >描述: {{ cat.description }}</v-card-subtitle>
+          <v-card-subtitle >健康状况: {{ cat.health_condition  == 1 ? "HEALTHY" : cat.health_condition == 2 ? "SICK" : cat.health_condition == 3 ? "VACCINATED" : "DEAD"}}</v-card-subtitle>
           <!-- <v-card-subtitle>收到投喂: {{ cat.cans }}</v-card-subtitle> -->
-          <v-card-text>
+          
+          <v-expand-transition>
+            <div v-show="showStates[cat.id]?.showCatEdit" class="mt-8">
+              <v-text-field
+                v-model="name"
+                label="姓名"
+                type="text"
+                :rules="[v => !!v || '昵称不能为空']"
+              ></v-text-field>
+              <v-text-field
+                v-model="description"
+                label="描述"
+                type="text"
+                :rules="[v => !!v || '描述不能为空']"
+              ></v-text-field>
+              <v-btn color="primary" @click="updateCatProfile(cat.id)">确认更新</v-btn>
+              <v-btn color="grey" class="ml-2" @click="toggleSection(cat.id, 'showCatEdit', false)">取消</v-btn>
+            </div>
+          </v-expand-transition>
+
+          <v-card-actions class="d-flex justify-center">
             <v-btn
               rounded="lg"
-              class="mr-2"
               variant="elevated"
-              color = "blue-darken-3"
+              color = "blue-lighten-1"
               @click="feedCat(cat)"
+              v-if="user.login && !user.is_superuser"
             >
               <v-icon left class = "mr-1">mdi-paw</v-icon>投喂
+            </v-btn>
+            <v-btn
+              rounded="lg"
+              variant="elevated"
+              color = "green-lighten-1"
+              @click= goToCatDetails(cat.id)
+              v-if="user.login"
+              >
+            <v-icon left class = "mr-1">mdi-cat</v-icon> 详情
+            </v-btn>
+            <v-btn
+                color="blue-lighten-1"
+                rounded="lg"
+                variant="elevated"
+                @click="toggleSection(cat.id, 'showCatEdit', true)"
+                v-if="user.login && user.is_superuser"
+              >
+              <v-icon left class="mr-1">mdi-pencil</v-icon> 修改信息
+            </v-btn>
+            <v-btn
+                color="purple-lighten-1"
+                rounded="lg"
+                variant="elevated"
+                @click="toggleSection(cat.id, 'showAvatarUpload', true)"
+                v-if="user.login && user.is_superuser"
+              >
+              <v-icon left class="mr-1">mdi-image</v-icon> 上传图片
             </v-btn>
             <v-btn 
               rounded="lg"
               @click="showDeleteDialog = true ; removeCatId = cat.id" 
               v-if="isAdmin" 
-              class="ml-2" 
               variant="elevated"
-              color="red darken-1">
-              <v-icon left class = "mr-1">mdi-delete</v-icon>删除
+              color="red-lighten-1">
+              <v-icon left class = "mr-1">mdi-delete</v-icon>删除猫咪
             </v-btn>
-          </v-card-text>
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
@@ -137,7 +183,7 @@
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { getProfile } from '@/api/user';
-import { getCats, createCat,deleteCat } from '@/api/cat';
+import { getCats, createCat,deleteCat,updateCatByAdmin } from '@/api/cat';
 import { user } from '@/api/user';
 import snackbar from '@/api/snackbar';
 
@@ -145,6 +191,74 @@ const cats = ref([]);
 const isAdmin = ref(false);
 const router = useRouter();
 const remainingCans = ref(6);
+const showStates = ref({});
+const name = ref('');
+const description = ref('');
+const showCatEdit = ref(false);
+
+onMounted(async() => {
+  fetchProfile();
+  const response = await getCats();
+  cats.value = response.cats;
+  cats.value.forEach((cat) => {
+    showStates.value[cat.id] = {
+      showCatEdit: false,
+      showAvatarUpload: false,
+    };
+  })
+});
+
+const toggleSection = (id, section, value) => {
+  Object.keys(showStates.value).forEach((key) => {
+    showStates.value[key][section] = key == id ? value : false;
+    console.log("key : " + key);
+    console.log("section : " + section);
+    console.log("toggleSection : " +  showStates.value[key][section]);
+  });
+
+  if (section === "showCatEdit" && value) {
+      // console.error(id)
+      const cat = cats.value.find((u) => u.id === id)
+      // console.error(cat)
+      name.value = cat.name;
+      description.value = cat.description;
+  }
+};
+
+const updateCatProfile = async (id) => {
+    try {
+      if (!name.value || name.value.trim() === '') {
+        snackbar.error('昵称或邮箱不能为空');
+        return;
+      }
+      if (name.value.length > 128) {
+        snackbar.error('姓名长度不能超过128个字符');
+        return;
+      }
+      if (!description.value || description.value.trim() === '') {
+        snackbar.error('描述不能为空');
+        return;
+      }
+      if (description.value.length > 256) {
+        snackbar.error('描述长度不能超过256个字符');
+        return;
+      }
+      const catData = {
+        name: name.value,
+        description: description.value,
+      };
+      console.error('catData', catData);
+      const response = await updateCatByAdmin(id , catData);
+      console.log('信息修改成功', response);
+      showCatEdit.value = false;
+      await fetchCats();
+      snackbar.success('信息修改成功');
+      toggleSection(id, "showCatEdit", false);
+    } catch (error) {
+      console.error('更新猫咪资料失败:', error);
+      snackbar.error('更新猫咪资料失败');
+    }
+  };
 
 const cat_in = ref({
   name: '',
@@ -191,8 +305,16 @@ const submitForm = async () => {
         description: cat_in.value.description,
       },
     };
+    if (cat_in.name.length > 128) {
+      snackbar.error('姓名长度不能超过128个字符');
+      return;
+    }
     if (cat_in.value.age > 30) {
       snackbar.error('年龄过大，请重新输入！');
+      return;
+    }
+    if (cat_in.value.description.length > 256) {
+      snackbar.error('描述长度不能超过256个字符');
       return;
     }
     const response = await createCat(data);
@@ -228,7 +350,12 @@ const fetchProfile = async () => {
 const fetchCats = async () => {
   try {
     const response = await getCats();
-    cats.value = response.cats;
+    const updatedData = response.cats;
+    console.error(updatedData)
+    cats.value = cats.value.map((cat) => {
+        const updatedCat = updatedData.find((u) => u.id === cat.id);
+        return updatedCat ? updatedCat : cat;
+    });
   } catch (error) {
     console.error('获取猫咪列表失败:', error);
   }
@@ -236,7 +363,7 @@ const fetchCats = async () => {
 
 const removeCat = async (id) => {
   try {
-    const response = await deleteCat(id);
+    await deleteCat(id);
     snackbar.success('删除成功！');
     cats.value = cats.value.filter((c) => c.id !== id);
     fetchCats();
@@ -263,8 +390,4 @@ const feedCat = (cat) => {
   }
 };
 
-onMounted(() => {
-  fetchProfile();
-  fetchCats();
-});
 </script>
